be442fe4b58663cd482dd4d60e15426f0984e2bc
Shekhar3742 <imderek3742@gmail.com>
2025-05-11 06:48:56 +0530

    Day1code

---
+++ Day-1/Day1.js
@@ -0,0 +1,45 @@
+// Activity 1
+
+var number1 = 5;
+console.log(number1);
+
+let string1 = 'Rahul';
+console.log(string1)
+
+// Activity 2
+
+const bool = true;
+console.log(bool)
+
+// Activity 3
+
+const number2 = 8; // js number are always one type=> double 64bit
+const string2 = "kumar";
+const bool2 = false;
+const person ={
+    firstname : "rahul",
+    lastname : "kumar"
+};
+
+const array = ["rahul", "Ayush", "badal"];
+console.log(typeof number2)
+console.log(typeof string2)
+console.log(typeof bool2)
+console.log(person.firstname)
+console.log(typeof person)
+console.log(array[0])
+console.log(typeof array)
+
+// Activity 4
+
+let x = 5;
+console.log(x);
+ x = 10;
+console.log(x);
+
+
+// Activity 5
+const x1 = 5;
+console.log(x);
+ x1 = 10; // we cant reassign a value of const again
+console.log(x);
